ce89b9f381b4fbbc47347e9ffb8792b68f8fb958
raafet lahmar <arabejs.ga@gmail.com>
2018-11-10 23:23:39 +0100

    v0.0.0.1.4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var request = require("request");
+var http = require('http');
 var fs = require('fs');
 
 var app = express();
@@ -9,75 +9,34 @@ var port = process.env.PORT || 8080;
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var options = {
-  method: 'POST',
-  url: 'http://thgss.com/index.php?p=download3',
-  qs: {
-    p: 'download3'
-  },
-  headers: {
-    'postman-token': 'ed4b48ee-0ae6-596d-8d4f-0b15f79eff0f',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded'
-  },
-  form: {
-    done: 'true'
-  }
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = http.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
 };
 
-var html = `
-<!DOCTYPE html>
-<html ng-app="RDapp" ng-controller="RDapp" lang="en" dir="ltr" >
-  <head>
-    <meta charset="utf-8">
-    <title>GO</title>
-  </head>
-  <body>
-    Raafet
-    <br>
-    <br>
-    <br>
-    <br>
-    <button type="button" name="button" ng-hide="btn" ng-click="up()">Update</button>
-    <span ng-hide="msg">Server is Update! :)</span>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.7.5/angular.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.7.5/angular-route.min.js"></script>
-    <script type="text/javascript">
-      var RD = angular.module('RDapp', ['ngRoute']);
-      RD.controller('RDapp', function($scope, $http) {
-        $scope.btn = false;
-        $scope.msg = true;
-
-        $scope.up = function () {
-          var settings = {"headers": {"content-type": "application/x-www-form-urlencoded"}};
-          var turl = 'http://thgss.com/index.php?p=download3';
-          $http.post(turl, $.param({'done': true}), settings)
-          .then(function(data) {
-            $http.post('http://iptv.raafet.ga/api', $.param({'data': data}), settings)
-            .then(function(data) {
-              console.log(data);
-              $scope.btn = true;
-              $scope.msg = false;
-            }, function (error) {
-              console.log(error);
-            });
-          }, function (error) {
-            console.log(error);
-          });
-        };
-      });
-    </script>
-  </body>
-</html>
-`;
-
-
-
 // routes will go here
+app.get('/add/:id', function(req, res) {
+  var data = req.params.id;
+  var url = "http://24.thgss.com:8000/get.php?username="+data+"&password="+data+"&type=m3u&output=mpegts";
+  download(url,'data.txt', function(err){
+    if (err) {
+      res.send(err);
+    }else{
+      res.send('<h1>OK!</h1>');
+    }
+  });
+});
+
 app.get('/', function(req, res) {
-  res.send(html);
+  res.send('Welcom!');
 });
 
 
@@ -90,13 +49,7 @@ app.get('/rd.m3u', function(req, res) {
 
 
 
-app.post('/api', function(req, res) {
-  var data = req.body.data;
-  fs.appendFile('data.txt', data, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
-});
+
 // start the server
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
